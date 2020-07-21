77c705969b079136bdcfc9580e455144e3147fc3
Mukhrit <mukhritgupta20@gmail.com>
2020-07-22 02:21:21 +0530

    tested route on postman

--- routes/postfeedback.js
+++ routes/postfeedback.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const Feedbacks = require('../models/feedmodal');
 
 const feedbackrouter = express.Router();
-const Feedbacks = require('../models/feedmodal');
+
 feedbackrouter.use(bodyParser.json());
-feedbackrouter.route('/')
-.post((req,res,next)=>{
-    Feedbacks.create(req.body)
-    .then((feed) => {
+feedbackrouter.route('/').post((req, res, next) => {
+  Feedbacks.create(req.body)
+    .then(
+      (feed) => {
         console.log('Feedback Created ', feed);
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.json(feed);
-    }, (err) => next(err))
+      },
+      (err) => next(err)
+    )
     .catch((err) => next(err));
-}
-);
-module.exports=feedbackrouter;
+});
+module.exports = feedbackrouter;
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ console.log('Starting the server');
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
 
 app.use('/posts', require('./routes/postRoutes'));
-app.use('/feedback',require('./routes/postfeedback'));
+app.use('/feedback', require('./routes/postfeedback'));
 
 console.log('Connecting to MongoDB');
 mongoose.connect(
@@ -27,12 +27,13 @@ mongoose.connect(
     console.log('MongoDB connection established');
   }
 );
-app.use(function(req, res, next) {
+
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
