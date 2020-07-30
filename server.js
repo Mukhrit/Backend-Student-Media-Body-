e03a172ddf5d05616e2b71e1b45000f90595944c
Rakshit Jain <rakshitdhoka13@gmail.com>
2020-07-31 01:37:13 +0530

    Build setup

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+var path = require('path');
 const bodyParser = require('body-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
@@ -14,11 +15,14 @@ app.use(bodyParser.json());
 const PORT = process.env.PORT || 5000;
 console.log('Starting the server');
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api/feedback', require('./routes/postfeedback'));
+app.use('/api/data', require('./routes/getHome'));
+app.use('/api/Blog',require('./routes/postBlog'));
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/public/index.html'));
+});
 
-app.use('/posts', require('./routes/postRoutes'));
-app.use('/feedback', require('./routes/postfeedback'));
-app.use('/data', require('./routes/getHome'));
-app.use('/Blog',require('./routes/postBlog'));
 
 console.log('Connecting to MongoDB');
 mongoose.connect(
