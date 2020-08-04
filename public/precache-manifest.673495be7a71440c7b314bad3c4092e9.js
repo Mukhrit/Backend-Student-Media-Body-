3214b5fd5e8df72ea1e8c74a3dad8c83db1edfb1
Rakshit Jain <rakshitdhoka13@gmail.com>
2020-08-04 16:52:36 +0530

    Done

---
+++ public/precache-manifest.673495be7a71440c7b314bad3c4092e9.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "695c9ba9cfbb82885456fbba8a6c4f59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faa947b77990f7ef2d08",
+    "url": "/static/css/2.6aea431c.chunk.css"
+  },
+  {
+    "revision": "02af2a014745bcbc215c",
+    "url": "/static/css/main.8b7fbee0.chunk.css"
+  },
+  {
+    "revision": "faa947b77990f7ef2d08",
+    "url": "/static/js/2.ebcec602.chunk.js"
+  },
+  {
+    "revision": "86085dda9b340c792a3e22a2aed48f01",
+    "url": "/static/js/2.ebcec602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02af2a014745bcbc215c",
+    "url": "/static/js/main.4bc44c5e.chunk.js"
+  },
+  {
+    "revision": "8ca0cc78878b9f7bb3b8",
+    "url": "/static/js/runtime-main.5b8a12d1.js"
+  },
+  {
+    "revision": "9efa09f1f6615777f8991e14de8b8568",
+    "url": "/static/media/divider-bg.9efa09f1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "c34270a9bd9d8a2d761f0630e502eb3e",
+    "url": "/static/media/logo2.c34270a9.png"
+  },
+  {
+    "revision": "2822bb19dfbf13244c195707eaa36719",
+    "url": "/static/media/photo-1.2822bb19.jpeg"
+  },
+  {
+    "revision": "3308d0a8dd7f4cd56d4e40ee2e656bc4",
+    "url": "/static/media/photo-2.3308d0a8.jpeg"
+  },
+  {
+    "revision": "ebfeb8b335b7711f78489ed6c7fcedb3",
+    "url": "/static/media/photo-4.ebfeb8b3.jpeg"
+  },
+  {
+    "revision": "76953c2b7ae073784d645117b533f44a",
+    "url": "/static/media/rohan.76953c2b.jpeg"
+  }
+]);
